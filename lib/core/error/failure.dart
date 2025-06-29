d58b1c79e89d59981917f2cfc0205c373bf89def
abdallah <abdallahtolba17@gmail.com>
2025-06-29 05:24:35 +0300

    add failure file

---
+++ lib/core/error/failure.dart
@@ -0,0 +1,18 @@
+abstract class Failure {
+  final String message;
+  const Failure(this.message);
+}
+
+class DatabaseFailure extends Failure {
+  const DatabaseFailure(super.message);
+}
+
+class NotFoundFailure extends Failure {
+  const NotFoundFailure(super.message);
+}
+
+// core/error/exceptions.dart
+class DatabaseException implements Exception {
+  final String message;
+  const DatabaseException(this.message);
+}
