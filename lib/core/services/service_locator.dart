8fd7d0a924ee409656e1c4fa92172275640bd213
abdallah <abdallahtolba17@gmail.com>
2025-06-29 05:52:41 +0300

    add service locator file

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,8 +1,28 @@
 import 'package:get_it/get_it.dart';
 import 'package:tick_note/core/data_source/local_data_source/shared_preferences/cache_helper.dart';
+import 'package:tick_note/core/data_source/local_data_source/sqflite/database_helper.dart';
+import 'package:tick_note/features/notes/data/data_source/notes_local_data_source.dart';
+import 'package:tick_note/features/notes/data/repository/notes_repo_implement.dart';
+import 'package:tick_note/features/notes/domain/repository/notes_repo.dart';
+import 'package:tick_note/features/notes/presentation/controller/cubit/notes_cubit.dart';
 
-final getIt = GetIt.instance;
+final sl = GetIt.instance;
 void setupServiceLocator() {
   //shared preferences
-  getIt.registerSingleton<CacheHelper>(CacheHelper());
+  sl.registerSingleton<CacheHelper>(CacheHelper());
+  // ==================== DatabaseHelper ====================
+  sl.registerLazySingleton<DatabaseHelper>(() => DatabaseHelper());
+
+  // ==================== Data Sources ====================
+  sl.registerLazySingleton<NotesLocalDataSource>(
+    () => NotesLocalDataSourceImpl(databaseHelper: sl()),
+  );
+
+  // ==================== Repositories ====================
+  sl.registerLazySingleton<NotesRepository>(
+    () => NotesRepositoryImpl(localDataSource: sl()),
+  );
+
+  // ==================== Cubits ====================
+  sl.registerFactory(() => NotesCubit(notesRepository: sl()));
 }
