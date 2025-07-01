a4845f0fbce767f9a01ed5f96468efeaacc4e204
abdallah <abdallahtolba17@gmail.com>
2025-07-01 20:17:42 +0300

    add SharedBottomNavigation

---
+++ lib/features/main/presentation/widgets/shared_bottom_nav.dart
@@ -0,0 +1,44 @@
+// 2. shared_bottom_nav.dart
+import 'package:flutter/material.dart';
+
+class SharedBottomNavigation extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  const SharedBottomNavigation({
+    Key? key,
+    required this.currentIndex,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 8,
+            offset: const Offset(0, -2),
+          ),
+        ],
+      ),
+      child: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: onTap,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.note_alt_outlined),
+            activeIcon: Icon(Icons.note_alt),
+            label: 'Notes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.check_box_outlined),
+            activeIcon: Icon(Icons.check_box),
+            label: 'Todo',
+          ),
+        ],
+      ),
+    );
+  }
+}
