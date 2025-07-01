9ebe094a61758fd7fedc12fd78f7e3bf6cfb4737
abdallah <abdallahtolba17@gmail.com>
2025-07-01 20:18:53 +0300

    add SharedFloatingActionButton

---
+++ lib/features/main/presentation/widgets/shared_fab.dart
@@ -0,0 +1,24 @@
+// 3. shared_fab.dart
+import 'package:flutter/material.dart';
+
+class SharedFloatingActionButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final IconData icon;
+  final String tooltip;
+
+  const SharedFloatingActionButton({
+    Key? key,
+    required this.onPressed,
+    required this.icon,
+    required this.tooltip,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: onPressed,
+      tooltip: tooltip,
+      child: Icon(icon),
+    );
+  }
+}
